afe647a103c4c4e30f1c270d87ee5476e9b3b642
takuto-abc <tmtydoraemon2@gmail.com>
2025-12-07 00:07:17 +0900

    v8: update openapi

---
+++ src/main/java/org/springframework/petmanagement/model/type/ItemType.java
@@ -0,0 +1,13 @@
+package org.springframework.petmanagement.model.type;
+
+public enum ItemType {
+    FOOD,
+    TREAT,
+    SUPPLEMENT,
+    PAD,
+    TOY,
+    COLLAR,
+    HYGIENE,
+    MEDICAL,
+    OTHER
+}
