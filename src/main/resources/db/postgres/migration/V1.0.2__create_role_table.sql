0affa33ab54efe1fd1a0102928ded1ff7b7b6ca7
takuto-abc <tmtydoraemon2@gmail.com>
2025-12-13 17:59:32 +0900

    fix migration

--- src/main/resources/db/postgres/migration/V1.0.2__create_role_table.sql
+++ src/main/resources/db/postgres/migration/V1.0.2__create_role_table.sql
@@ -8,13 +8,13 @@ CREATE TYPE role_type AS ENUM (
 );
 
 CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
+    id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name role_type NOT NULL UNIQUE
 );
 
 CREATE TABLE user_roles (
-    user_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
+    user_id UUID NOT NULL,
+    role_id UUID NOT NULL,
     PRIMARY KEY (user_id, role_id),
     CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_user_roles_role FOREIGN KEY (role_id) REFERENCES roles (id)
